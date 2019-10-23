f50b9fefa86083d6877d26ac8214e2c03e6b26d4
Somiacao <18210240048@fudan.edu.cn>
2019-10-23 09:59:07 +0800

    hello world

---
+++ src/main/java/com/meiyingying/springlaunch/SpringlaunchApplication.java
@@ -0,0 +1,12 @@
+package com.meiyingying.springlaunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlaunchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringlaunchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meiyingying/springlaunch/SpringlaunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meiyingying.springlaunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlaunchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
