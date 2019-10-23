121356f80e7b0fc4a8a7e701a23468cfbed20019
Somiacao <18210240048@fudan.edu.cn>
2019-10-23 10:39:59 +0800

    hello world modfied

--- src/main/java/com/meiyingying/springlaunch/controller/HelloController.java
+++ src/main/java/com/meiyingying/springlaunch/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
+        System.out.println("hello world");
         return "Hello World";
     }
 }
