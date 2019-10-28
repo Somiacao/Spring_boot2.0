7613568f85540bbfb4e7c363eb882c0fe285f2f8
Somiacao <18210240048@fudan.edu.cn>
2019-10-28 19:39:43 +0800

    配置文件注入值数据校验

--- src/main/java/com/meiyingying/springlaunch/model/yaml/Family.java
+++ src/main/java/com/meiyingying/springlaunch/model/yaml/Family.java
@@ -5,12 +5,17 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @Component
+@Validated
 @ConfigurationProperties(prefix = "family")
 public class Family {
 //    @Value("${family.family-name}")
+    @NotEmpty
     private String familyName;
 
     private Father father;
