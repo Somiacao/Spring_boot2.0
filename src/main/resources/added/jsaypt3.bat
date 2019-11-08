dea26f9e56518fb3ad4a500b871a8d47a3b4ff30
Somiacao <18210240048@fudan.edu.cn>
2019-11-08 16:06:51 +0800

    加密

---
+++ src/main/resources/added/jsaypt3.bat
@@ -0,0 +1,6 @@
+@echo off
+set/p input=待加密的明文字符串:
+set/p password=加密密钥(盐值):
+echo 加密中......
+java -cp jasypt-1.9.0.jar org.jasypt.intf.cli.JasyptPBEStringEncryptionCLI input=%input% password=%password% algorithm=PBEWithMD5AndDES
+pause
